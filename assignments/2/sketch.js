c649d37df83d493cfdd05c3121c7a753f87ad375
Ignacio Streuly <ijstreuly@gmail.com>
2016-09-27 01:19:01 -0400

    Add: Refactor

--- assignments/2/sketch.js
+++ assignments/2/sketch.js
@@ -1,22 +1,19 @@
 var gameInitiated = false;
-
 var xPos, yPos;
 var compXPos, compYPos;
 var song;
-var compXSpeed = 5;
-var compYSpeed = 4;
 var WIDTH = $(window).width();
 var HEIGHT = $(window).height();
-
-var FULL_WIDTH = 2*WIDTH;
-var xDesired, yDesired;
-var lineIncrement = 0;
 
 //user
 var userWidth=25;
 var userHeight=25;
+var playerXSpeed = 10;
+var playerYSpeed = 10;
 
 //computer
+var compXSpeed = 1;
+var compYSpeed = 1;
 var computerWidth = 50;
 var computerHeight = 50;
 
@@ -25,146 +22,144 @@ var playerScore = 0;
 var computerScore = 0;
 var bounces = 0;
 var newRound = false;
-
 var playerPositions = [[100,100], [WIDTH-100,100], [100,HEIGHT-100], [WIDTH-100,HEIGHT-100]];
 var computerPositions = [[300,300], [WIDTH-300,100], [300,HEIGHT-300], [300,300]];
+
+
 
 function preload() {
   characterImage = loadImage("assets/user_character.svg");
   computerImage = loadImage("assets/user_character.svg");
+  fontRegular = loadFont("assets/Lato-Regular.ttf");
   // computerImage = loadImage("assets/computer_character.svg");
 }
 
-
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-
+  //initial positions
   xPos = 100 - 0.5 * characterImage.width;
   yPos = 100 - 0.5 * characterImage.height;
   compXPos = 250 - 0.5 * computerImage.width;;
   compYPos = 250 - 0.5 * computerImage.height;;
-  
-   // default the character to want to move to the middle of the screen
-  xDesired = 250;
-  yDesired = 250;
 }
-
-
 
 function mousePressed(){
   gameInitiated = true;
 }
 
+function drawBackground(){
+    background("#F8F8F8");
+}
 
-function draw() {    
-    // movement logic
-        if (keyIsDown(LEFT_ARROW)) {
-          xPos -= 10;
-        }
-        if (keyIsDown(RIGHT_ARROW)) {
-          xPos += 10;
-        }
-        if (keyIsDown(UP_ARROW)) {
-          yPos -= 10;
-        }
-        if (keyIsDown(DOWN_ARROW)) {
-          yPos += 10;
-        }
+function draw() {
+    compXPos += compXSpeed;
+    compYPos += compYSpeed;
+
+    var userRight = xPos + characterImage.width;
+    var userLeft  = xPos;
+    var userTop   = yPos;
+    var userBottom = yPos + characterImage.height;
+
+    var computerTop    = compYPos;
+    var computerBottom = compYPos + computerImage.height;
+    var computerRight = compXPos + computerImage.width;
+    var computerLeft  = compXPos;
+
+
+    // chase logic
+    if (userRight < computerLeft) {
+        // left
+        if (compXSpeed < 0)
+            compXSpeed *=-1
+    }
+
+    if (userLeft > computerRight) {
+    // right
+        if (compXSpeed > 0)
+            compXSpeed*= -1
+    }
+    
+    if (userBottom < computerTop) {
+        // top
+        if (compYSpeed < 0)
+            compYSpeed *= -1
+    }
+    if (userTop > computerBottom) {
+        // bottom
+        if (compYSpeed > 0)
+            compYSpeed *= -1
+    }
+
+
+    //player movement
+    if (keyIsDown(LEFT_ARROW)) {
+      xPos -= playerXSpeed;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      xPos += playerXSpeed;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      yPos -= playerYSpeed;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      yPos += playerYSpeed;
+    }
     
     //wrap around
-        if (xPos > width) {
-          xPos = 0;
-        }
-        if (xPos < 0) {
-          xPos = width;
-        }
-        if (yPos > HEIGHT) {
-          yPos = 0;
-        }
-        if (yPos < 0) {
-          yPos = HEIGHT;
-        }
+    if (xPos > width) {
+      xPos = 0;
+    }
+    if (xPos < 0) {
+      xPos = width;
+    }
+    if (yPos > HEIGHT) {
+      yPos = 0;
+    }
+    if (yPos < 0) {
+      yPos = HEIGHT;
+    }
 
 
-    //computer player
+    //computer score
+    if (compXPos > width) {
+        computerScore++;
+        newRound = true;
+    }
+    if (compXPos < 0) {
+        computerScore++;
+        newRound = true;
+    }
+    if (compYPos > HEIGHT) {
+        computerScore++;
+        newRound = true;
+    }
+    if (compYPos < 0) {
+        computerScore++;
+        newRound = true;
+    }
 
-        compXPos += compXSpeed;
-        compYPos += compYSpeed;
+    //collisions
+    if (userRight < computerLeft || userLeft > computerRight || userBottom < computerTop || userTop > computerBottom) {
 
-        if (compXPos > width) {
-            computerScore++;
-            newRound = true;
-        }
-        if (compXPos < 0) {
-            computerScore++;
-            newRound = true;
-        }
-        if (compYPos > HEIGHT) {
-            computerScore++;
-            newRound = true;
-        }
-        if (compYPos < 0) {
-            computerScore++;
-            newRound = true;
-        }
-
-         //collisions
-          var userRight = xPos + characterImage.width;
-          var userLeft  = xPos;
-          var userTop   = yPos;
-          var userBottom = yPos + characterImage.height;
-
-          var computerTop    = compYPos;
-          var computerBottom = compYPos + computerImage.height;
-          var computerRight = compXPos + computerImage.width;
-          var computerLeft  = compXPos;
-
-          // deterine if a collision has occurred
-          noStroke();
-          if (userRight < computerLeft || 
-            userLeft > computerRight || 
-            userBottom < computerTop || 
-            userTop > computerBottom) {
-            
-
-          }
-          else {
-            // collision
-            playerScore++;
-            newRound = true;
-          }
-
-          if (userRight < computerLeft) {
-            // left
-            if (compXSpeed < 0)
-                compXSpeed *=-1
-
-          }
-          if (userLeft > computerRight) {
-            // right
-            if (compXSpeed > 0)
-                compXSpeed*= -1
-
-          }
-          if (userBottom < computerTop) {
-            // top
-            if (compYSpeed < 0)
-                compYSpeed *= -1
-          }
-          if (userTop > computerBottom) {
-            // bottom
-            if (compYSpeed > 0)
-                compYSpeed *= -1
-          }
+    }
+    else {
+        // collision
+        playerScore++;
+        newRound = true;
+    }
 
     //drawing logic
     if (gameInitiated) {
         drawBackground();
-
         fill(0);
-        text("Time played: " + Number((millis()/1000).toFixed(1)), 50, 85);
-        text("your score: " + playerScore, 50, 110);
-        text("computer score: " + computerScore, 50, 130);
+        stroke(255);
+        textSize(15);
+        textFont(fontRegular);
+        textStyle(NORMAL);
+
+        text("Time played: " + Number((millis()/1000).toFixed(1)), 50, 45);
+        text("Your score: " + playerScore, 50, 69);
+        text("Computer score: " + computerScore, 50, 92);
 
         fill("#44B78B");
 
@@ -200,11 +195,11 @@ function draw() {
         }
        
     } else {
-        text("Intro text: click to start", 50, 50);
-        
+        stroke(255);
+        textSize(20);
+        textFont(fontRegular);
+        textStyle(NORMAL);
+        text("Catch the green thing before it hits the corner of the screen.", 50, 70)
+        text("Click to start.", 50, 105)
     }
-}
-
-function drawBackground(){
-    background("#F8F8F8");
 }
